33086fa51515f43ba3581c441d853ef49db55038
Tristen Gadzinski <tgadz@DESKTOP-OP00JIL>
2024-03-04 13:54:57 -0600

    Created javascript and html file

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+1.) Create folder
+2.) create files in folder
+3.) link index and js files together
+4.) use arthimetic operators and variables in js to create lock combination
+5.) use const to get each number and string
+6.) use alert to get message to pop up on screen
+*/
+
+//made first lock combo
+const num1 = 100 / 10;
+
+// made second lock combo
+const num2 = num1 + 30;
+
+//made third lock combo
+const num3 = 13 * 3;
+
+//made message into string
+const str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//made an alert to disply message and combo
+alert(`${str1} ${num1} ${num2} ${num3}`);
+
